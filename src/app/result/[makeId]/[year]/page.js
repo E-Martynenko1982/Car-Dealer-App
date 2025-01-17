b6e279bda8292a546444c418a635686c0d46592b
E-Martynenko1982 <motor8103@gmail.com>
2025-01-17 22:11:58 +0200

    update

--- src/app/result/[makeId]/[year]/page.js
+++ src/app/result/[makeId]/[year]/page.js
@@ -1,6 +1,25 @@
 import ModelsClient from "./ModelsClient";
 import { Suspense } from "react";
 
+export async function generateStaticParams() {
+  const makesUrl = process.env.NEXT_PUBLIC_API_GET_MAKES;
+  const res = await fetch(makesUrl);
+  if (!res.ok) {
+    throw new Error(`HTTP error! status: ${res.status}`);
+  }
+  const data = await res.json();
+  const makes = data.Results || [];
+
+  const currentYear = new Date().getFullYear();
+  const years = Array.from({ length: currentYear - 2015 + 1 }, (_, i) => 2015 + i);
+
+  const paths = makes.flatMap(make =>
+    years.map(year => ({ params: { makeId: make.MakeId.toString(), year: year.toString() } }))
+  );
+
+  return paths;
+}
+
 export default async function ResultPage({ params }) {
   const { makeId, year } = await params;
   const modelsUrlBase = process.env.NEXT_PUBLIC_API_GET_MODELS_BASE;
@@ -23,5 +42,3 @@ export default async function ResultPage({ params }) {
     </Suspense>
   );
 }
-
-
